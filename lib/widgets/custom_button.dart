73ff38aea4f21f28e26ebe0a319f8282f84aec5d
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2026-01-16 18:34:53 +0800

    Update custom_button.dart

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,31 +1,41 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_starter_kit/core/app_colors.dart';
 
-class CustomButton extends StatelessWidget {
-  final String text;
+class ActionButton extends StatelessWidget {
+  final String title;
   final VoidCallback onPressed;
-
-  const CustomButton({
-    required this.text,
-    required this.onPressed,
-    super.key,
-  });
+  const ActionButton({super.key, required this.title, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.primary,
-          padding: const EdgeInsets.symmetric(vertical: 14),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+    return ElevatedButton(
+      style: ButtonStyle(
+        shape: WidgetStatePropertyAll(
+          ContinuousRectangleBorder(
+            side: const BorderSide(
+              color: AppColors.secondaryColor,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(16.r)),
           ),
         ),
-        onPressed: onPressed,
-        child: Text(text, style: const TextStyle(fontSize: 16)),
+        backgroundColor: const WidgetStatePropertyAll(AppColors.secondaryColor),
+      ),
+      onPressed: onPressed,
+      child: SizedBox(
+        width: 180.w,
+        height: 60.h,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: AppColors.fontLight,
+              fontFamily: 'montserrat',
+              fontSize: 16.sp,
+            ),
+          ),
+        ),
       ),
     );
   }
